067741b60545821bd7678ede3fa32bbeb3d288eb
Mariano Alvira <mar@devl.org>
2011-09-10 15:44:51 -0400

    use contikimac

--- platform/redbee-econotag/contiki-conf.h
+++ platform/redbee-econotag/contiki-conf.h
@@ -119,9 +119,9 @@ typedef unsigned long rtimer_clock_t;
 #if WITH_UIP6
 /* Network setup for IPv6 */
 #define NETSTACK_CONF_NETWORK sicslowpan_driver
-#define NETSTACK_CONF_MAC     nullmac_driver 
-/*#define NETSTACK_CONF_RDC     contikimac_driver*/ /* contikimac for redbee hasn't been well tested */
-#define NETSTACK_CONF_RDC     nullrdc_driver
+#define NETSTACK_CONF_MAC     csma_driver 
+#define NETSTACK_CONF_RDC     contikimac_driver /* contikimac for redbee hasn't been well tested */
+//#define NETSTACK_CONF_RDC     nullrdc_driver
 #define NETSTACK_CONF_RADIO   contiki_maca_driver
 #define NETSTACK_CONF_FRAMER  framer_802154
 
@@ -135,7 +135,7 @@ typedef unsigned long rtimer_clock_t;
 
 #define NETSTACK_CONF_NETWORK rime_driver
 #define NETSTACK_CONF_MAC     csma_driver
-#define NETSTACK_CONF_RDC     sicslowmac_driver
+#define NETSTACK_CONF_RDC     contikimac_driver
 #define NETSTACK_CONF_RADIO   contiki_maca_driver
 #define NETSTACK_CONF_FRAMER  framer_802154
 
